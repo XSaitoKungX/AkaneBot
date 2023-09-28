ff9fbb1c5c2f4c11680b262e3614248f1d20a285
XSaitoKungX <64774999+XSaitoKungX@users.noreply.github.com>
2023-09-28 23:40:00 +0200

    Create GoogleImageHandler.cs

---
+++ Engine/ImageHandler/GoogleImageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Akane.Engine.ImageHandler
+{
+    internal class GoogleImageHandler
+    {
+        public Dictionary<int, string> images = new Dictionary<int, string>();
+        public static readonly GoogleImageHandler googleImageHandler = new GoogleImageHandler();
+
+        static GoogleImageHandler() { }
+
+        public string GetImageAtId(int id)
+        {
+            var imageURL = images.TryGetValue(id, out var image);
+            return image;
+        }
+    }
+}
