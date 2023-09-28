2125803a09ba5897e7feb0af6f3d5ff149302d0e
XSaitoKungX <64774999+XSaitoKungX@users.noreply.github.com>
2023-09-28 23:29:04 +0200

    Create SwearFilter.cs

---
+++ Engine/SwearFilter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akane.Engine
+{
+    internal class SwearFilter
+    {
+        public List<string> filter = new List<string>();
+        public SwearFilter()
+        {
+            filter.Add("Arschloch");
+            filter.Add("Asshole");
+            filter.Add("Bastard");
+            filter.Add("Bastart");
+            filter.Add("Bitch");
+            filter.Add("Cocksucker");
+            filter.Add("Fuck");
+            filter.Add("Fick");
+            filter.Add("Ficken");
+            filter.Add("Ficker");
+            filter.Add("gefickt");
+            filter.Add("Hure");
+            filter.Add("Hurensohn");
+            filter.Add("Hurensöhne");
+            filter.Add("Hurentochter");
+            filter.Add("Motherfucker");
+            filter.Add("Mushi");
+            filter.Add("Muschi");
+            filter.Add("Mutterficker");
+            filter.Add("Nutte");
+            filter.Add("Nutten");
+            filter.Add("Penis");
+            filter.Add("Penissucker");
+            filter.Add("Pisser");
+            filter.Add("Pussy");
+            filter.Add("Schlampe");
+            filter.Add("Schwanz");
+            filter.Add("Schwänze");
+            filter.Add("Schwanzlutscher");
+            filter.Add("Schwuchtel");
+            filter.Add("Vagina");
+            filter.Add("Vollpfosten");
+            filter.Add("Vollfosten");
+            filter.Add("Wichser");
+            filter.Add("Wixer");
+            filter.Add("Wixxer");
+        }
+    }
+}
