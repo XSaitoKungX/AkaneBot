6166caaf150dd5497f1c40fb01a9642c5e4f2683
XSaitoKungX <64774999+XSaitoKungX@users.noreply.github.com>
2023-09-28 23:38:04 +0200

    Update CardBuilder.cs

--- Engine/ExternalClasses/CardBuilder.cs
+++ Engine/ExternalClasses/CardBuilder.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Akane.Engine.ExternalClasses
 {
     internal class CardBuilder
     {
+        private int[] cardNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+        private string[] cardSuits = { "Kreuz", "Pik", "Karo", "Herz" };
+
+        public int SelectedNumber { get; set; }
+        public string SelectedCard {  get; set; }
+
+        public CardBuilder()
+        {
+            var Random = new Random();
+            int indexNumbers = Random.Next(0, this.cardNumbers.Length - 1);
+            int indexSuit = Random.Next(0, this.cardSuits.Length - 1);
+
+            this.SelectedNumber = this.cardNumbers[indexNumbers];
+            this.SelectedCard = $"{this.cardNumbers[indexNumbers]} {this.cardSuits[indexSuit]}";
+        }
     }
 }
