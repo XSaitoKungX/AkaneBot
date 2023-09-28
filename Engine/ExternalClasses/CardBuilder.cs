7e5d17bf146f5d63d52989b6fc812c14ab78f21e
XSaitoKungX <64774999+XSaitoKungX@users.noreply.github.com>
2023-09-28 23:36:08 +0200

    Create CardBuilder.cs

---
+++ Engine/ExternalClasses/CardBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akane.Engine.ExternalClasses
+{
+    internal class CardBuilder
+    {
+    }
+}
