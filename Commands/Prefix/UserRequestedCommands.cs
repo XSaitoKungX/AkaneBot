96e8d5a41623473dfbe6dad4be253b5dab1659ae
XSaitoKungX <64774999+XSaitoKungX@users.noreply.github.com>
2023-10-01 14:16:51 +0200

    Update UserRequestedCommands.cs

--- Commands/Prefix/UserRequestedCommands.cs
+++ Commands/Prefix/UserRequestedCommands.cs
@@ -8,8 +8,11 @@ using OpenAI_API;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
+using static Google.Apis.Requests.BatchRequest;
 
 namespace Akane.Commands.Prefix
 {
@@ -26,8 +29,9 @@ namespace Akane.Commands.Prefix
             int IDCount = 0;
 
             // Replace with your own Custom Search Engine ID and API Key
-            string cseId = "CSE-ID-HERE";
-            string apiKey = "API-Key-HERE";
+            // https://cse.google.com/cse.js?cx=34e1a705bcc3a4f02
+            string cseId = "34e1a705bcc3a4f02";
+            string apiKey = "AIzaSyBjSnKalOVzUwTech1xK67u9HxlGP1aVeQ";
 
             // Initialise the API
             var customSearchService = new CustomSearchAPIService(new BaseClientService.Initializer
@@ -96,29 +100,54 @@ namespace Akane.Commands.Prefix
         [Command("chatgpt")]
         public async Task ChatGPT(CommandContext ctx, params string[] message)
         {
-            // Initialise the API
-            var api = new OpenAIAPI("API-KEY-HERE");
-
-            // Initialise a new Chat
-            var chat = api.Chat.CreateConversation();
-            chat.AppendSystemMessage("Gib eine Suchanfrage ein.");
-
-            // Pass in the Query to ChatGPT
-            chat.AppendUserInput(string.Join(" ", message));
-
-            // Get the response
-            string response = await chat.GetResponseFromChatbot();
-
-            // Show in Discord Embed Message
-            var randomColor = new DiscordColor((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
-            var responseMsg = new DiscordEmbedBuilder()
+            // Stelle sicher, dass eine Nachricht eingegeben wurde
+            if (message.Length == 0)
             {
-                Title = string.Join(" ", message),
-                Description = response,
-                Color = randomColor
-            };
+                await ctx.RespondAsync("Du musst eine Nachricht eingeben, damit ich antworten kann.");
+                return;
+            }
 
-            await ctx.Channel.SendMessageAsync(responseMsg);
+            try
+            {
+                // API-Schlüssel für OpenAI
+                string apiKey = "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";
+
+                // Initialisiere die OpenAIAPI
+                var api = new OpenAIAPI(apiKey);
+
+                // Initialisiere eine neue Chat-Konversation
+                var chat = api.Chat.CreateConversation();
+                chat.AppendSystemMessage("Gib eine Suchanfrage ein.");
+
+                // Füge die Benutzereingabe als Nachricht hinzu
+                chat.AppendUserInput(string.Join(" ", message));
+
+                // Hole die Antwort von ChatGPT
+                string response = await chat.GetResponseFromChatbot();
+
+                // Sende die Antwort als Discord Embed Message
+                var randomColor = new DiscordColor((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
+                var responseMsg = new DiscordEmbedBuilder()
+                {
+                    Title = string.Join(" ", message),
+                    Description = response,
+                    Color = randomColor
+                };
+
+                await ctx.RespondAsync(embed: responseMsg);
+            }
+            catch (Exception ex)
+            {
+                // Behandle Fehler und gebe eine Fehlermeldung aus
+                var randomColor = new DiscordColor((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256));
+                var errorMessage = new DiscordEmbedBuilder()
+                {
+                    Title = "Ein Fehler ist aufgetreten:",
+                    Description = ex.Message,
+                    Color = randomColor
+                };
+                await ctx.RespondAsync(embed: errorMessage);
+            }
         }
     }
 }
